c8b30ed3e08f46a42b8e4979f7dd4c20f08620d7
navya0247 <122387960+navya0247@users.noreply.github.com>
2026-02-18 02:28:11 +0530

    Delete src directory

--- src/routes/attachment.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAttachment } from '../controllers/attachment.controller';
-import { protect } from '../middleware/auth.middleware';
-
-const router = express.Router();
-
-router.get('/:id', protect, getAttachment);
-
-export default router;
